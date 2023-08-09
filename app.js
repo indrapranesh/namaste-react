a8ec4de292fb10ebd9c05f1d175f1f316ceebaf7
indrapranesh <indrapranesh2111@gmail.com>
2023-08-09 20:22:34 +0530

    Use React to manipulate the DOM to create nested elements

---
+++ app.js
@@ -0,0 +1,12 @@
+const element = React.createElement("div", {
+    id: "heading",
+}, [
+    React.createElement("h1", {id: "heading1"}, "First"),
+    React.createElement("div", {}, [
+        React.createElement("h2", {id: "heading2"}, "Second"),
+        React.createElement("h3", {id: "heading3"}, "Third")
+    ])
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(element);
